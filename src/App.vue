56666498bfc4f69e23d76ff4e064add21a86a675
Zuegelwagen <m.inglin@gmx.ch>
2024-07-14 13:39:47 +0200

    first componentcreated

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
-<div id="app"></div>
+  <BaseCard>
+    <h1>This is a title</h1>
+    <p>This is some content inside the card.</p>
+  </BaseCard>
 </template>
 
 <script>
+import BaseCard from "./components/BaseCard.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    BaseCard,
+  },
 };
 </script>
 
 <style>
 body {
   margin: 0;
+
 }
 #app {
-  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011f35 100%);
   min-height: 100vh;
   width: 100vw;
+  padding: 100px;
+  box-sizing: border-box;
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011f35 100%);
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="base-card">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BaseCard',
+  };
+  </script>
+  
+  <style scoped>
+  .base-card {
+    background-color: #011F35;
+    border-radius: 16px;
+    padding: 20px 32px 20px 32px;
+  }
+  </style>
+  
